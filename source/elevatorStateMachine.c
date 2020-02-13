b233163dd787238c18aaf1c8542795d4fb0c4c05
Selsebil <radiselsebil@gmail.com>
2020-02-13 17:43:19 +0100

    Changed the function elevator_safety_function(). Made it more readable and added more functionality. The function timer is also added, but not finished yet.
    Signed-off-by: Selsebil <radiselsebil@gmail.com>

--- source/elevatorStateMachine.c
+++ source/elevatorStateMachine.c
@@ -12,39 +12,69 @@ ElevatorOrder g_queue[6];
 double g_floor;
 
 void destination_reached(double floor){
-	hardware_command_door_open(1); //skal være åpen i 3 sekunder. timer(3)?
+	hardware_command_door_open(1); 
 	hardware_command_floor_indicator_on(floor);
 	hardware_command_order_light(floor, HARDWARE_ORDER_DOWN, 0);
 	hardware_command_order_light(floor, HARDWARE_ORDER_UP, 0);
 	hardware_command_order_light(floor, HARDWARE_ORDER_INSIDE, 0);
-	popQueue(g_queue);
+	deleteOrdersOnFloor(g_queue, g_queue_length, floor);
+
+	timer(3);
+	hardware_command_door_open(0);
 
 }
+
 
 void elevator_safety_function(){
-	if (hardware_read_stop_signal()){
-		hardware_command_stop_light(1);
-		hardware_command_movement(HARDWARE_MOVEMENT_STOP);
-		clearQueue(g_queue);
-		if (hardware_read_floor_sensor(0)|| hardware_read_floor_sensor(1)||hardware_read_floor_sensor(2)|| hardware_read_floor_sensor(3)){ //en definert etasje. skrive på en annen måte?
-			g_state = DOOR_OPEN; //???
-			hardware_command_door_open(1);
+	switch (hardware_read_stop_signal())
+	{
+	case 1:
+	clearQueue(g_queue);
+	hardware_command_movement(HARDWARE_MOVEMENT_STOP);
+	hardware_command_stop_light(1);
+		while(hardware_read_stop_signal()){
+			if (hardware_read_floor_sensor(0)|| hardware_read_floor_sensor(1)||hardware_read_floor_sensor(2)|| hardware_read_floor_sensor(3)){
+				hardware_command_door_open(1);
+				g_state=DOOR_OPEN;
+			}
 		}
+		hardware_command_stop_light(0);
+		g_state=STANDING_STILL;
+		timer(3);
+		hardware_command_door_open(0);
+		break;
+	case 0:
+		if (g_state!=DOOR_OPEN){
+			break;
+		}
+		hardware_command_door_open(1);
+		while(hardware_read_obstruction_signal()==1){
+		}
+		
+		timer(3);
+		hardware_command_door_open(0);
+		break;
+	default:
+		break;
 	}
 }
 
+
 void empty_queue(){
-	if (checkQueue(g_queue)){
+	if (checkQueue(g_queue)==0){
 		g_state = STANDING_STILL;
+		hardware_command_door_open(0);
 	}
 }
+
+
 
 int checkQueue(ElevatorOrder * queue){
 	if ((queue[0].floor==-1)&&(queue[0].orderType==NONE)){
 		return 0;
 	}
 	else{
-	return 1;
+		return 1;
 	}
 }
 
@@ -67,15 +97,31 @@ void elevator_init(){
 		}
 		
 	}
+	HardwareOrder order_types[3] = {
+        HARDWARE_ORDER_UP,
+        HARDWARE_ORDER_INSIDE,
+        HARDWARE_ORDER_DOWN
+    };
+
+    for(int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
+        for(int i = 0; i < 3; i++){
+            HardwareOrder type = order_types[i];
+            hardware_command_order_light(f, type, 0);
+        }
+    }
+
 	hardware_command_floor_indicator_on(g_floor);
 	State=STANDING_STILL;
 	clearQueue(g_queue);
 	}
-	
-void timer(int seconds){
+
+
+
+// denne er ikke ferdig.
+void timer(int milliseconds){
     while(1){
-        seconds--;
-		sleep(1);
+        milliseconds--;
+		sleep(0.001);
         if (hardware_read_obstruction_signal()==1|| hardware_read_stop_signal()==1){
 			break;
 		}
