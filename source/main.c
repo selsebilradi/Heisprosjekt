607682bce7164e4d8b1d3823727db485e6de3c8c
Gruppe58 <support@google.com>
2020-02-20 20:13:24 +0100

    we made a main function!
    Signed-off-by: Gruppe58 <support@google.com>

--- source/main.c
+++ source/main.c
@@ -1,63 +1,7 @@
-#define _DEFAULT_SOURCE
-
-
-#include <stdio.h>
-#include <stdlib.h>
-#include "hardware.h"
-#include <unistd.h>
 #include "elevatorStateMachine.h"
-#include "queue.h"
 
-void timer(int milliseconds){
-	int time=milliseconds;
-    while(1){
-        milliseconds--;
-		usleep(1);
-        if (hardware_read_obstruction_signal()==1|| hardware_read_stop_signal()==1){
-            hardware_command_floor_indicator_on(2);
-			timer(time);
-			break;
-		}
-		if (milliseconds==0){
-            hardware_command_floor_indicator_on(1);
-			break;
-		}
-
-    }
-}
 
 int main(){
-    int error = hardware_init();
-    if(error != 0){
-        fprintf(stderr, "Unable to initialize hardware\n");
-        exit(1);
-    }
-
-    printf("=== Example Program ===\n");
-    printf("Press the stop button on the elevator panel to exit\n");
-    lights();
-
-    hardware_command_movement(HARDWARE_MOVEMENT_UP);
-
-    while(1){
-        if(hardware_read_stop_signal()){
-            hardware_command_movement(HARDWARE_MOVEMENT_STOP);
-            break;
-        }
-
-        if(hardware_read_floor_sensor(0)){
-            hardware_command_movement(HARDWARE_MOVEMENT_UP);
-        }
-        if(hardware_read_floor_sensor(HARDWARE_NUMBER_OF_FLOORS - 1)){
-            hardware_command_movement(HARDWARE_MOVEMENT_DOWN);
-        }
-	if(hardware_read_order(1,HARDWARE_ORDER_UP)){
-	   hardware_command_order_light(1, HARDWARE_ORDER_UP,1);
-	   hardware_command_movement(HARDWARE_MOVEMENT_STOP);
-	   timer(30000);	
-	   hardware_command_order_light(1, HARDWARE_ORDER_UP,0);
-	   hardware_command_movement(HARDWARE_MOVEMENT_UP);
-	
-	}
-    }
+	FSM();
+	return 0;
 }
