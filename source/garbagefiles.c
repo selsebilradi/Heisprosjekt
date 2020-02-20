ab984495cd0c2b59132094488547cb9d3b0c4808
EmbLab <support@google.com>
2020-02-20 17:51:51 +0100

    deleted popQueue()
    Signed-off-by: EmbLab <support@google.com>

--- source/garbagefiles.c
+++ source/garbagefiles.c
@@ -92,3 +92,12 @@ void printQueue(ElevatorOrder* queue, int length){
 	}
 	printf(" }\n\n");
 }
+
+void popQueue(ElevatorOrder* queue, int length){
+	for (int i = 0; i < length-1; i++){
+		queue[i].floor = queue[i+1].floor;
+		queue[i].orderType = queue[i+1].orderType;
+	}
+	queue[length-1].floor = -1;
+	queue[length-1].orderType=HARDWARE_ORDER_INSIDE;
+}
