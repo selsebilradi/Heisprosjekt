d2bbc1cfcfdebf1fdd4a562f754498c0274ce5ce
Selsebil <radiselsebil@gmail.com>
2020-02-25 16:53:11 +0100

    changed so we always use camelCase in our own code
    Signed-off-by: Selsebil <radiselsebil@gmail.com>

--- source/elevatorStateMachine.c
+++ source/elevatorStateMachine.c
@@ -78,7 +78,7 @@ void elevatorInit(){
 		}
 	}
 
-	HardwareOrder order_types[3] = {
+	HardwareOrder orderTypes[3] = {
         HARDWARE_ORDER_UP,
         HARDWARE_ORDER_INSIDE,
         HARDWARE_ORDER_DOWN
@@ -86,7 +86,7 @@ void elevatorInit(){
 
     for(int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
         for(int i = 0; i < 3; i++){
-            HardwareOrder type = order_types[i];
+            HardwareOrder type = orderTypes[i];
             hardware_command_order_light(f, type, 0);
         }
     }
@@ -176,7 +176,7 @@ void updateCurrentFloor(){
 }
 
 void clearAllOrderLights(){
-    HardwareOrder order_types[3] = {
+    HardwareOrder orderTypes[3] = {
         HARDWARE_ORDER_UP,
         HARDWARE_ORDER_INSIDE,
         HARDWARE_ORDER_DOWN
@@ -184,7 +184,7 @@ void clearAllOrderLights(){
 
     for(int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
         for(int i = 0; i < 3; i++){
-            HardwareOrder type = order_types[i];
+            HardwareOrder type = orderTypes[i];
             hardware_command_order_light(f, type, 0);
         }
     }
